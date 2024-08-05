10ae84e07e808b253457836e6505c197a702b09a
kael <Kaizune897531@gmail.com>
2024-08-05 01:46:21 -0400

    added save as funtion
    as well as created some comments and fixed naming convections

--- markdown-app/main.go
+++ markdown-app/main.go
@@ -4,9 +4,12 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 )
 
+// config holds information that we can share with any function
+// that has a receiver of type *config
 type config struct {
 	EditWidget    *widget.Entry
 	PreviewWidget *widget.RichText
@@ -24,21 +27,24 @@ func main() {
 	- Set the content of the window to the user interface
 	- Show the window
 	*/
-
 	// Create a new fyne app
-	a := app.New()
+	//a := app.New()
 
-	// Create a new window
-	win := a.NewWindow("Markdown Editor")
+	// create a fyne app
+	a := app.NewWithID("my markdown editor")
 
-	// Get the user interface
+	// create a window for the app
+	win := a.NewWindow("Markdown")
+
+	// get the user interface
 	edit, preview := cfg.makeUI()
+	cfg.createMenuItems(win)
 
-	// Set the content of the window to the user interface
+	// set the content of the window
 	win.SetContent(container.NewHSplit(edit, preview))
 
-	// Show the window
-	win.Resize(fyne.Size{Width: 800, Height: 600})
+	// show window and run app
+	win.Resize(fyne.Size{Width: 800, Height: 500})
 	win.CenterOnScreen()
 	win.ShowAndRun()
 }
@@ -84,16 +90,66 @@ func (app *config) makeUI() (*widget.Entry, *widget.RichText) {
 // 6. Sets the created main menu to the provided window.
 func (app *config) createMenuItems(win fyne.Window) {
 
+	// create three menu items
 	openMenuItem := fyne.NewMenuItem("Open...", func() {})
-
 	saveMenuItem := fyne.NewMenuItem("Save", func() {})
+	app.SaveMenuItem = saveMenuItem
+	app.SaveMenuItem.Disabled = true
+	saveAsMenuItem := fyne.NewMenuItem("Save as...", app.saveAsFunc(win))
 
-	saveAsMenuItem := fyne.NewMenuItem("Save As...", func() {})
-
+	// create a file menu, and add the three items to it
 	fileMenu := fyne.NewMenu("File", openMenuItem, saveMenuItem, saveAsMenuItem)
 
+	// create a main menu, and add the file menu to it
 	menu := fyne.NewMainMenu(fileMenu)
 
+	// set the main menu for the application
 	win.SetMainMenu(menu)
+}
 
+// saveAsFunc returns a function that opens a "Save As" dialog to save the content of the EditWidget to a file.
+//
+// Parameters:
+// - app: a pointer to the config struct which holds the application's configuration and state.
+// - win: the window to which the dialog will be attached.
+//
+// Returns:
+// - func(): a function that opens the "Save As" dialog.
+//
+// The function performs the following steps:
+//  1. Creates a new file save dialog with a callback function to handle the file saving process.
+//  2. In the callback function:
+//     a. If an error occurs, it shows an error dialog and returns.
+//     b. If the user cancels the save operation (write is nil), it simply returns.
+//     c. Otherwise, it writes the content of the EditWidget to the selected file.
+//     d. Updates the CurrentFile field in the config struct with the URI of the saved file.
+//     e. Closes the file writer after writing.
+//     f. Updates the window title to include the name of the saved file.
+//     g. Enables the Save menu item.
+//  3. Shows the save dialog.
+func (app *config) saveAsFunc(win fyne.Window) func() {
+	return func() {
+		saveDialog := dialog.NewFileSave(func(write fyne.URIWriteCloser, err error) {
+			if err != nil {
+				dialog.ShowError(err, win)
+				return
+			}
+
+			if write == nil {
+				// user cancelled
+				return
+			}
+
+			// save file
+			write.Write([]byte(app.EditWidget.Text))
+			app.CurrentFile = write.URI()
+
+			defer write.Close()
+
+			win.SetTitle(win.Title() + " - " + write.URI().Name())
+			app.SaveMenuItem.Disabled = false
+
+		}, win)
+		saveDialog.Show()
+	}
 }
